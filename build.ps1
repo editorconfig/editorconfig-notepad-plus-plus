5dabf4c1675f43a7ea868b598cdccabe22fc7766
Michael Kriese <michael.kriese@visualon.de>
2017-08-11 18:02:48 +0200

    Added build scripts for windows x86 and x64.

---
+++ build.ps1
@@ -0,0 +1,126 @@
+param(
+    [ValidateSet("pcre2", "core", "npp", "all")]
+    [string] $proj,
+
+    [switch] $init,
+
+    [switch] $install,
+
+    [ValidateSet("Debug", "Release")]
+    [string] $config = "Release",
+
+    [ValidateSet("x86", "x64")]
+    [string] $arch = "x64",
+
+
+    [ValidateSet("15","14","12")]
+    [int] $vsver = 15
+)
+
+$ErrorActionPreference = "stop"
+
+
+function exec {
+    param
+    (
+        [ScriptBlock] $ScriptBlock,
+        [int[]] $AllowedExitCodes = @(0)
+    )
+
+    $backupErrorActionPreference = $script:ErrorActionPreference
+
+    $script:ErrorActionPreference = "Continue"
+    try {
+        & $ScriptBlock
+        if ($AllowedExitCodes -notcontains $LASTEXITCODE) {
+            throw "Execution failed with exit code $LASTEXITCODE"
+        }
+    }
+    finally {
+        $script:ErrorActionPreference = $backupErrorActionPreference
+    }
+}
+
+if ($proj -eq "all") {
+    .\build.ps1 -proj pcre2 -init:$init -install:$install -arch $arch -config $config
+    .\build.ps1 -proj core -init:$init -install:$install -arch $arch -config $config
+    .\build.ps1 -proj npp -init:$init -install:$install -arch $arch -config $config
+    return
+}
+
+$PREFIX = "../build"
+
+$dest = "bin"
+
+if ((Test-Path $dest) -ne $true) {
+    throw "Missing build path! Used init?"
+}
+
+if ($init) {
+
+    $gen = "Visual Studio "
+
+    switch ($vsver) {
+        15 {
+            $gen += "15 2017"
+        }
+        14 {
+            $gen += "14 2015"
+        }
+        12 {
+            $gen += "12 2013"
+        }
+        default {
+            throw "Visual Studio version $vsver not supported!"
+        }
+    }
+
+    if ($arch -eq "x64") {
+        $gen += " Win64"
+    }
+
+    New-Item $dest\$arch\$proj -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+    Push-Location $dest\$arch\$proj
+    try {
+        switch ($proj) {
+            pcre2 {
+                exec { cmake -G "$gen" -DCMAKE_INSTALL_PREFIX="$PREFIX" `
+                        -DPCRE2_STATIC_RUNTIME=ON `
+                        -DPCRE2_BUILD_PCRE2GREP=OFF `
+                        -DPCRE2_BUILD_TESTS=OFF `
+                        "../../pcre2" }
+            }
+            core {
+                exec { cmake -G "$gen" -DCMAKE_INSTALL_PREFIX="$PREFIX" `
+                        -DPCRE2_STATIC=ON `
+                        "../../editorconfig-core-c" }
+            }
+            npp {
+                exec { cmake -G "$gen" -DEDITORCONFIG_CORE_PREFIX="$(Resolve-Path $PREFIX)" "../../../." }
+            }
+        }
+    }
+    finally {
+        Pop-Location
+    }
+}
+
+if ((Test-Path $dest\$arch\$proj) -ne $true) {
+    throw "Missing build path! Used init?"
+}
+
+exec { cmake --build $dest\$arch\$proj `-- /p:Configuration=$config }
+
+if ($install) {
+    switch ($proj) {
+        pcre2 {
+            exec { cmake --build $dest\$arch\$proj --target install `-- /p:Configuration=$config }
+        }
+        core {
+            exec { cmake --build $dest\$arch\$proj --target install `-- /p:Configuration=$config }
+        }
+        npp {
+            Copy-Item $dest\$arch\$proj\bin\unicode\$config\NppEditorConfig.dll -Destination $dest\$arch\ -Force -Confirm:$false
+        }
+    }
+}
