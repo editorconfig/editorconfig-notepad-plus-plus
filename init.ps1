9bc118929b00c6c125e35e9bb54ad1c9a5c3d87d
Frank Fesevur <fesevur@gmail.com>
2023-11-21 21:38:31 +0100

    Add `-Dynamic` to hopefully fix the download in GitHub Actions

--- init.ps1
+++ init.ps1
@@ -19,7 +19,7 @@ $url = "https://github.com/PCRE2Project/pcre2/releases/download/pcre2-$($pcre)/p
 $output = "$dest\pcre-$($pcre).zip"
 
 "Downloading pcre2 v$pcre sources" | Write-Host -ForegroundColor DarkGreen
-Start-BitsTransfer -Source $url -Destination $output
+Start-BitsTransfer -Source $url -Destination $output -Dynamic
 
 "Extracting pcre2 v$pcre sources" | Write-Host -ForegroundColor DarkGreen
 Expand-Archive -Path $output -DestinationPath $dest
@@ -33,7 +33,7 @@ $url = "https://github.com/editorconfig/editorconfig-core-c/archive/v$($edc).zip
 $output = "$dest\editorconfig-core-c-$($edc).zip"
 
 "Downloading editorconfig-core-c v$edc sources" | Write-Host -ForegroundColor DarkGreen
-Start-BitsTransfer -Source $url -Destination $output
+Start-BitsTransfer -Source $url -Destination $output -Dynamic
 
 "Extracting editorconfig-core-c v$edc sources" | Write-Host -ForegroundColor DarkGreen
 Expand-Archive -Path $output -DestinationPath $dest
